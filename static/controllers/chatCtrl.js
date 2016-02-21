2fa7fedaac03303768fa101e62988d3a27f8ca6d
Amanuel <amanuelb43@gmail.com>
2016-02-20 21:05:42 -0500

    Update chatCtrl.js

--- static/controllers/chatCtrl.js
+++ static/controllers/chatCtrl.js
@@ -1,5 +1,28 @@
-app.controller('chatCtrl', ["$scope","ref",
-  function($scope,ref){
+app.controller('chatCtrl', ["$scope","$firebaseObject", function($scope,$firebaseObject) {
 
-  }]
-)
+
+        var ref = new Firebase('https://sparke.firebaseio.com/')
+        var syncObject = $firebaseObject(ref);
+        $scope.usernameProfile = ''
+        $scope.imageProfile = ''
+
+
+        ref.onAuth(function(authData) {
+            ref.child("UsersAuthInfo").child(authData.uid).on("value", function(snapshot) {
+                $scope.usernameProfile = snapshot.val().Username;
+                $scope.imageProfile = snapshot.val().Image
+                console.log(snapshot.val().Image)
+                console.log(snapshot.val())
+                console.log($scope.usernameProfile)
+                var username = $scope.usernameProfile
+                console.log($scope.imageProfile)
+            })
+        })
+
+
+
+console.log($scope.data)
+        console.log($scope.usernameProfile)
+        console.log($scope.imageProfile)
+
+    }])
